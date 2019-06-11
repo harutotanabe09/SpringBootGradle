a652ae80bd985bd87e42c7da0d1fc977a966b270
Haruto Tanabe <harutotanabe@HarutonoMacBook-Air.local>
2019-06-11 23:59:49 +0900

    bin delete

--- bin/main/META-INF/sample/demo/dao/OriginDao/selectAll.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    *
-FROM
-    public.web_origins
-WHERE
-    1 = 1
-/*%if (criteria.clientId != null && criteria.clientId != 0) */
-AND client_id = /* criteria.clientId */1
-/*%end*/
--- bin/main/META-INF/sample/demo/dao/OriginDao/selectById.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-  *
-FROM
-  public.web_origins
-WHERE
-  client_id = /* id */1
